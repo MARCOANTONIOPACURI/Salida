ce449852c82dc1994bb94dac620986ce0ff7881f
MARCOANTONIOPACURI <134654386+MARCOANTONIOPACURI@users.noreply.github.com>
2023-10-13 18:57:09 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('UwU me asombra que puedas xD broma; Sabia que ibas a tener tiempo y no ibas a ser falla 🤗 yo tampoco sere falla, nos vemos 😁')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function () {
+    alert('Tranqui Fabi, mejor en vacaciones la tacita de te u otro dia, no hay problema entiendo 😊')
+})
